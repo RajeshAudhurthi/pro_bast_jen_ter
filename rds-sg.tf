deb70e5e546096f7fa2e797b5fbabe4c412c6e4a
RajeshAudhurthi <raajesh404@gmail.com>
2021-09-03 10:18:06 +0530

    rds ingress changed

--- rds-sg.tf
+++ rds-sg.tf
@@ -13,3 +13,10 @@ resource "aws_security_group_rule" "mysql_inbound_access" {
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
 }
+ingress {
+    description = "allow Bastain"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.sg_bastion_host.id]
+  }
