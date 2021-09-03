b748739d3ce540b8850ed845e4d8e79ee237657e
RajeshAudhurthi <raajesh404@gmail.com>
2021-09-03 10:29:51 +0530

    rds sg modified

--- rds-sg.tf
+++ rds-sg.tf
@@ -1,22 +1,16 @@
 resource "aws_security_group" "rds-sg" {
-  name   = "my-rds-sg"
-  vpc_id = aws_vpc.vpc.id
+  depends_on = [
+    aws_vpc.vpc,
+  ]
+  name        = "my-rds-sg"
+  description = "Allow Bastain inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
 
-}
-
-# Ingress Security Port 3306
-resource "aws_security_group_rule" "mysql_inbound_access" {
-  from_port         = 3306
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.rds-sg.id}"
-  to_port           = 3306
-  type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-ingress {
+  ingress {
     description = "allow Bastain"
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
     security_groups = [aws_security_group.sg_bastion_host.id]
   }
+}
